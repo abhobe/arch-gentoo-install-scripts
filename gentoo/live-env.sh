4f50e9849e23aff9d49825da6d6dbbf7769cfc8a
Arjun Bhobe <arjun.a.bhobe@gmail.com>
2021-01-04 11:33:25 -0800

    Update live-env.sh

--- gentoo/live-env.sh
+++ gentoo/live-env.sh
@@ -3,11 +3,13 @@ cfdisk
 mkfs.fat -F32 /dev/sda1
 mkfs.ext4 /dev/sda2
 mount /dev/sda2 /mnt/gentoo
+wget https://raw.githubusercontent.com/abhobe/arch-gentoo-install-scripts/master/gentoo/make.conf
 cd /mnt/gentoo
 wget https://bouncer.gentoo.org/fetch/root/all/releases/amd64/autobuilds/20210103T214503Z/stage3-amd64-20210103T214503Z.tar.xz
 mv stage3-amd64-20210103T214503Z.tar.xz  stage3-amd64.tar.xz
+tar xpvf stage3-amd64.tar.xz --xattrs-include='*.*' --numeric-owner
 mv  /mnt/gentoo/etc/portage/make.conf /mnt/gentoo/etc/portage/make.conf.orig
-cp make.conf /mnt/gentoo/etc/portage/make.conf
+cp /root/make.conf /mnt/gentoo/etc/portage/make.conf
 cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
 mount --types proc /proc /mnt/gentoo/proc
 mount --rbind /sys /mnt/gentoo/sys
