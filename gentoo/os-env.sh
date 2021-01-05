35e7fcf909a25a20d29f9e920d9b4e52612406c5
Arjun Bhobe <arjun.a.bhobe@gmail.com>
2021-01-04 19:01:42 -0800

    Added bootloader config to os environement

--- gentoo/os-env.sh
+++ gentoo/os-env.sh
@@ -29,5 +29,5 @@ emerge --ask sys-apps/mlocate
 emerge --ask net-misc/dhcpcd
 echo 'GRUB_PLATFORMS="efi-64"' >> /etc/portage/make.conf
 emerge --ask sys-boot/grub:2
-#grub-install --target=x86_64-efi --efi-directory=/bootyes
-#grub-mkconfig -o /boot/grub/grub.cfg
+grub-install --target=x86_64-efi --efi-directory=/boot
+grub-mkconfig -o /boot/grub/grub.cfg
